1ab5bdcfab714f18a992c0f932777c96d298744a
salfarot-cloud <salfarot@alumnes.santjosepobrer.es>
2026-05-19 10:46:09 +0200

    feat: implement base website layout, routing, navbar, and mock pages for AUTOESCUELA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Reservas } from './pages/reservas/reservas';
+import { Test } from './pages/test/test';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'reservas', component: Reservas },
+  { path: 'test', component: Test },
+  { path: '**', redirectTo: '' }
+];
